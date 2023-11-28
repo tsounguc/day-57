9683e7664389a116989113fbcc132ed22d976fa1
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-28 10:13:21 -0500

    Use jinja to produce dynamic html pages

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+import random
+
 from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(name=None):
+    random_number = random.randint(1, 10)
+    copyright_year = datetime.now().year
+    return render_template("index.html", name=name, num=random_number, copyright_year=copyright_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
